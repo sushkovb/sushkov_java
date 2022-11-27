d8ca05355bf5795e4d5284034fe583ebdbb1e29c
borissuskov <boris.sushkov@gmail.com>
2022-11-27 17:42:18 +0300

    added first task

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    // 1. Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+    public static void getNumber(int number) {
+        if (number > 7) {
+            System.out.println("Привет");
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        Main.getNumber(9);
+    }
+}
