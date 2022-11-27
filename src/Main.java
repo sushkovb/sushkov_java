a5878f90afa7c4d478bd0aa4d8c2ae64fcf7763a
borissuskov <boris.sushkov@gmail.com>
2022-11-27 17:46:47 +0300

    added second task

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,23 @@ public class Main {
         if (number > 7) {
             System.out.println("Привет");
         }
-
     }
 
-
+    // Составить алгоритм: если введенное имя совпадает с Вячеслав,
+    // то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+    public static void name(String name) {
+        if (name == "Вячеслав") {
+            System.out.println("Привет, " + name);
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
 
 
     public static void main(String[] args) {
 
         Main.getNumber(9);
+        Main.name("Вячеслав");
+
     }
 }
