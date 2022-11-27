3ca7741a741166e15ea62f1f44271709f0373e22
borissuskov <boris.sushkov@gmail.com>
2022-11-27 17:49:39 +0300

    added third task

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         }
     }
 
-    // Составить алгоритм: если введенное имя совпадает с Вячеслав,
-    // то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+    // 2. Составить алгоритм: если введенное имя совпадает с Вячеслав,
+    //    то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
     public static void name(String name) {
         if (name == "Вячеслав") {
             System.out.println("Привет, " + name);
@@ -16,11 +16,21 @@ public class Main {
         }
     }
 
+    // 3. Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
+    public static void massiveOfNumbers() {
+        int numbers[]=new int[] {3, 4, 5, 6, 7, 8, 9, 21};
+
+        for (int i = 0; i < numbers[i]; i++)
+            if(numbers[i] % 3 == 0)
+
+                System.out.println(numbers[i]);
+    }
 
     public static void main(String[] args) {
 
         Main.getNumber(9);
         Main.name("Вячеслав");
+        Main.massiveOfNumbers();
 
     }
 }
